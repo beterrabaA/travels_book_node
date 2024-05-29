8272990f9cb1aa50af4853cec47d365ffa5c11af
Beterraba <ls6182315@gmail.com>
2024-05-29 20:10:35 -0300

    chore: use dotenv to get .env informations

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import {
   createProduct,
 } from "./src/controllers/productController.js";
 import { client } from "./src/infra/database/client/client.js";
+import "dotenv/config";
 
 const server = createServer((req, res) => {
   if (req.url === "/api/products" && req.method === "GET") {
@@ -31,5 +32,8 @@ client
   .catch((e) => console.log(e.message));
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+client.on("notification", (msg) => console.log(msg));
+client.on("notice", (msg) => console.warn("notice:", msg));
 
 server.on("connection", (socket) => console.log("someone connected!"));
