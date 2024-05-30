17b40c651cfe7ea12a6b1d30799bd394fedfc6f9
Beterraba <ls6182315@gmail.com>
2024-05-30 17:54:38 -0300

    feat: Add query function to database client

--- src/infra/database/client/client.js
+++ src/infra/database/client/client.js
@@ -1,5 +1,9 @@
 import pg from 'pg'
 import { config } from '../config/config.js'
 const { Client } = pg
- 
+
 export const client = new Client(config)
+
+export const query = (text, params, callback) => {
+    return client.query(text, params, callback)
+}
