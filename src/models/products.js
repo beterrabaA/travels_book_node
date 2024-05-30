d977385c83bc7c2f194138ae9042d327a0e31aa0
Beterraba <ls6182315@gmail.com>
2024-05-30 17:57:48 -0300

    feat: Update products.js to use database queries instead of local data

--- src/models/products.js
+++ src/models/products.js
@@ -1,25 +1,36 @@
-import products from "../data/products.json" assert { type: "json" };
-import { v4 as uuid4 } from "uuid";
-import { writeDataFromFile } from "../utils/index.js";
+import { query } from "../infra/database/client/client.js";
 
 export function findAllProducts() {
   return new Promise((resolve, reject) => {
-    resolve(products);
+    const queryText = 'SELECT * FROM books;'
+    const productsDb = query(queryText)
+    resolve(productsDb);
+    reject([])
   });
 }
 
 export function findProductById(id) {
   return new Promise((resolve, reject) => {
-    const product = products.find((p) => p.id === id);
+    const queryText = 'SELECT * FROM books WHERE id = $1'
+    const product = query(queryText,[id])
     resolve(product);
   });
 }
 
 export function create(data) {
   return new Promise((resolve, reject) => {
-    const newProduct = { id: uuid4(), ...data };
-    products.push(newProduct)
-    writeDataFromFile(products)
+    const { title, description, price } = data
+    const queryText = 'INSERT INTO books(name, description, price) VALUES ($1, $2, $3);'
+    const queryValues = [title, description, price]
+    const newProduct = query(queryText, queryValues)
+    resolve(newProduct);
+  });
+}
+
+export function update(id) {
+  return new Promise((resolve, reject) => {
+    const queryText = 'SELECT * FROM books WHERE id = $1'
+    const product = query(queryText,[id])
     resolve(product);
   });
 }
