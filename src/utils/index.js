535c980042ad7b992a9625fc9ed83e5a7c82d01f
Beterraba <ls6182315@gmail.com>
2024-05-30 17:43:53 -0300

    remove unused function and products.json

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export function writeDataFromFile(content) {
-  fs.writeFileSync("./src/data/products.json", JSON.stringify(content), "utf-8", (err) => {
-    if (err) {
-        console.log("something went wrong writing products file");
-    }
-  });
-}
