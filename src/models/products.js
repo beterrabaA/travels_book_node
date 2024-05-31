84a0e12707331f3d867f5ae4aabf2431f75e47db
Beterraba <ls6182315@gmail.com>
2024-05-31 20:11:23 -0300

    refactor: Update products.js to use database queries instead of local data

--- src/models/products.js
+++ src/models/products.js
@@ -12,25 +12,35 @@ export function findAllProducts() {
 export function findProductById(id) {
   return new Promise((resolve, reject) => {
     const queryText = 'SELECT * FROM books WHERE id = $1'
-    const product = query(queryText,[id])
+    const product = query(queryText, [id])
     resolve(product);
   });
 }
 
 export function create(data) {
   return new Promise((resolve, reject) => {
-    const { title, description, price } = data
+    const { name, description, price } = data
     const queryText = 'INSERT INTO books(name, description, price) VALUES ($1, $2, $3);'
-    const queryValues = [title, description, price]
+    const queryValues = [name, description, price]
     const newProduct = query(queryText, queryValues)
     resolve(newProduct);
   });
 }
 
-export function update(id) {
+export function update(id, data) {
   return new Promise((resolve, reject) => {
-    const queryText = 'SELECT * FROM books WHERE id = $1'
-    const product = query(queryText,[id])
+    const { name, description, price } = data
+    const queryText = 'UPDATE books SET name = $1, description = $2, price = $3 WHERE id = $4;'
+    const product = query(queryText, [name, description, price, id])
     resolve(product);
   });
+}
+
+export function deleteProductById(id) {
+  return new Promise((resolve, reject) => {
+    const queryText = 'DELETE FROM books WHERE id = $1;'
+    query(queryText, [id])
+    resolve(1)
+    reject(0)
+  })
 }
