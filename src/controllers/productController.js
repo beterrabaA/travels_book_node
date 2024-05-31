ecdea38fdda5b5a4af0efeca8ba40740577d934c
Beterraba <ls6182315@gmail.com>
2024-05-31 15:51:23 -0300

    feat: Update productController to use utility functions for HTTP requests and responses

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,14 +3,14 @@ import {
   findProductById,
   create,
 } from "../models/products.js";
+import { getRequestData, sendResponse } from "../utils/index.js";
 
 // @desc gets all products
 export async function getProducts(request, response) {
   try {
     const result = await findAllProducts();
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(result.rows));
-  } catch (error) { }
+    sendResponse(response, 200, result.rows);
+  } catch (error) {}
 }
 
 export async function getProduct(request, response, id) {
@@ -23,19 +23,20 @@ export async function getProduct(request, response, id) {
       response.writeHead(200, { "Content-Type": "application/json" });
       response.end(JSON.stringify(product.rows[0]));
     }
-  } catch (error) { }
+  } catch (error) {}
 }
 
 export async function createProduct(request, response) {
   try {
-    const product = {
-      title: "Game of thrones",
-      description: "A book which tell a story",
-      price: 9.99,
-    };
+    const body = await getRequestData(request);
+    const { title, description, price } = JSON.parse(body);
+
+    const product = { title, description, price };
 
     await create(product);
     response.writeHead(201, { "Content-Type": "application/json" });
     response.end(JSON.stringify({ message: "product created!" }));
-  } catch (error) { }
+  } catch (error) {
+    console.log(error);
+  }
 }
