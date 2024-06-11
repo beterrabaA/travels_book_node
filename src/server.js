98d7e75863d7bd76a4dca3f7dc3fda2830a54523
Beterraba <ls6182315@gmail.com>
2024-06-11 15:16:27 -0300

    refactor: Add route for retrieving a travel by ID and update server.js

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ import { getProducts, createProduct } from "./controllers/productController.js";
 import { productRoutes } from "./routes/products.js";
 import "dotenv/config";
 import { getTravels } from "./controllers/travelController.js";
+import { travelRoutes } from "./routes/travels.js";
 
 const PORT = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
   const baseRoteMatch = req.url === "/api/products"
-  const urlMatcher = req.url.match(/\/api\/products\/([0-9]+)/)
+  const productUrlMatcher = req.url.match(/\/api\/products\/([0-9]+)/)
+  const travelUrlMatcher = req.url.match(/\/api\/travels\/([0-9]+)/)
   const method = req.method;
   const allowedMethods = ["GET", "PUT", "DELETE"];
   const travelsBaseRoute = req.url === "/api/travels";
@@ -20,12 +22,17 @@ const server = createServer((req, res) => {
   } else if (travelsBaseRoute && method === "GET") {
     getTravels(req, res);
 
-  } else if (urlMatcher && allowedMethods.includes(method)) {
-    const id = req.url.split("/")[3];
-    const route = productRoutes[method];
+  } else if (productUrlMatcher && allowedMethods.includes(method)) {
+    const productId = req.url.split("/")[3];
+    const routeForProduct = productRoutes[method];
 
-    route(req, res, id);
+    routeForProduct(req, res, productId);
 
+  } else if (travelUrlMatcher && allowedMethods.includes(method)) {
+    const travelId = req.url.split("/")[3];
+    const routeForTravel = travelRoutes[method]
+
+    routeForTravel(req, res, travelId)
   } else if (baseRoteMatch && method === "POST") {
     createProduct(req, res);
 
