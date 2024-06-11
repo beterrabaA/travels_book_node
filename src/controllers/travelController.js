1be72736858835ed55ed85c3e5b1fc0ed383e38b
Beterraba <ls6182315@gmail.com>
2024-06-11 15:08:52 -0300

    refactor: Add getTravelByIdService function and route for retrieving a travel by ID

--- src/controllers/travelController.js
+++ src/controllers/travelController.js
@@ -1,10 +1,22 @@
 import { findAllTravels } from "../models/travels.js";
+import { getTravelByIdService } from "../services/travelService.js";
 import { sendResponse } from "../utils/index.js";
 
 export async function getTravels(request, response) {
     try {
         const result = await findAllTravels();
         sendResponse(response, 200, result.rows);
+    } catch (error) {
+        sendResponse(response, 400, { message: error.message });
+    }
+}
+
+export async function getTravel(request, response, id) {
+    try {
+        const expanded = true
+        const optimized = true
+        const travel = await getTravelByIdService(id, expanded, optimized)
+        sendResponse(response, 200, travel);
     } catch (error) {
         sendResponse(response, 400, { message: error.message });
     }
