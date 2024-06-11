f94f25261cddbb5d6240144d26f2a58ab20abdf0
Beterraba <ls6182315@gmail.com>
2024-06-11 17:29:03 -0300

    refactor: Update travelService.js to import necessary modules and retrieve data from Rick and Morty API

--- src/services/travelService.js
+++ src/services/travelService.js
@@ -1,9 +1,12 @@
-import { findTravel } from "../models/travels"
+import { findTravel } from "../models/travels.js"
+import { requestRickAndMortyData } from "../utils/index.js"
 
 export const getTravelByIdService = async (id, expanded, optimized) => {
     const result = await findTravel(id)
     const travel = result.rows[0]
     if (!travel) throw new Error('product not found')
 
+    const rickData = await requestRickAndMortyData(travel.travel_stops)
+    console.log(rickData);
     return travel
 }
